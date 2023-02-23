47d6df195fec6eaabb34163b2f667ffe0cd31259
Yaqub Alwan <yaxxie@users.noreply.github.com>
2023-02-23 21:03:01 +0000

    black and mypyu fixes

--- good_bot/good_bot.py
+++ good_bot/good_bot.py
@@ -1,14 +1,12 @@
 import dataclasses
 import logging
+import typing
 
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler, Updater
 
 # Set up logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
@@ -20,21 +18,20 @@ class Args:
     chat_id: str = ""
 
 
-def greet_new_user(update: Update, context: CallbackContext):
+def greet_new_user(update: Update, context: CallbackContext) -> None:
     # Get the new user's name
     new_user = update.message.new_chat_members[0]
     new_user_name = new_user.first_name
 
     # Get the greeting message from the context
     bot_message = "Blip blop"
-    greeting_message = context.chat_data.get('greeting_message', 'Hello')
+    greeting_message = context.chat_data.get("greeting_message", "Hello")
 
     # Send the greeting message to the chat
     update.message.reply_text(f"[{bot_message}] {greeting_message}, {new_user_name}!")
 
 
-def server(args: Args):
-
+def server(args: Args) -> typing.NoReturn:
     # Create the bot instance
     updater = Updater(token=args.bot_id, use_context=True)
 
@@ -45,10 +42,12 @@ def server(args: Args):
     updater.dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members, greet_new_user), group=1)
 
     # Add the greeting message to the chat data
-    updater.dispatcher.chat_data['greeting_message'] = args.greeting
+    updater.dispatcher.chat_data["greeting_message"] = args.greeting
 
     # Start the bot
     updater.start_polling()
 
     # Run the bot until it is stopped
     updater.idle()
+
+    raise RuntimeError("Server stopped!")
